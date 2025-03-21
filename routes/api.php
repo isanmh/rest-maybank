21ddfa2566828f960411e3815e05dc808c6a7700
isanmh <1157050074@student.uinsgd.ac.id>
2025-03-21 13:20:16 +0700

    Add: initial init project API

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('test-api', function () {
+    return response()->json([
+        'status' => 200,
+        'message' => 'Hai, ini adalah response dari API $name dan $job',
+    ]);
+});
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
+Route::put('products/{id}', [ProductController::class, 'update']);
